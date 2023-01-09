a01c91b6cd45149e1fcff76e3d28746ff81b931b
f-porto <felipe.portosa@gmail.com>
2023-01-08 21:47:04 -0300

    expressions

--- src/ast.rs
+++ src/ast.rs
@@ -1,105 +1,193 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Statement<'a> {
     Nothing,
-    Assignment,
-    FunctionCall,
-    Label { identifier: &'a str },
+    Assignment {
+        vars: Vec<Expression<'a>>,
+        expressions: Vec<Expression<'a>>,
+    },
+    FunctionCall {
+        expression: Expression<'a>,
+        args: Vec<Expression<'a>>,
+    },
+    Label {
+        identifier: &'a str,
+    },
     Break,
-    Goto { identifier: &'a str },
-    Do { block: Block<'a> },
+    Goto {
+        identifier: &'a str,
+    },
+    Do {
+        block: Block<'a>,
+    },
     While {
-        condition: Expression,
+        condition: Expression<'a>,
         block: Block<'a>,
     },
     Repeat {
-        condition: Expression,
+        condition: Expression<'a>,
         block: Block<'a>,
     },
     If {
-        condition: Expression,
+        condition: Expression<'a>,
         consequence: Block<'a>,
         alternative: Box<Option<Statement<'a>>>,
     },
     NumericFor {
-        var: Expression,
-        start: Expression,
-        limit: Expression,
-        step: Option<Expression>,
+        var: Expression<'a>,
+        start: Expression<'a>,
+        limit: Expression<'a>,
+        step: Option<Expression<'a>>,
         block: Block<'a>,
     },
     GenericFor {
-        vars: Vec<Expression>,
-        exprs: Vec<Expression>,
+        vars: Vec<Expression<'a>>,
+        exprs: Vec<Expression<'a>>,
         block: Block<'a>,
     },
-    FunctionDecl,
-    LocalFunctionDecl,
-    LocalAttrs,
+    FunctionDecl {
+        expression: Expression<'a>,
+        args: Vec<Expression<'a>>,
+    },
+    LocalFunctionDecl {
+        expression: Expression<'a>,
+        args: Vec<Expression<'a>>,
+    },
+    LocalAttrs {
+        attrs: Vec<Expression<'a>>,
+        expressions: Vec<Expression<'a>>,
+    },
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Block<'a> {
     statements: Vec<Statement<'a>>,
 }
 
-#[derive(Debug)]
-pub enum Expression {
-    Variable {
-        value: String,
+#[derive(Debug, PartialEq)]
+pub enum Expression<'a> {
+    Nil,
+    Bool(bool),
+    Number(Number),
+    String(&'a str),
+    Table(Table),
+    VarArgs,
+    FunctionDef {
+        args: Vec<Expression<'a>>,
+        block: Block<'a>,
     },
-    Number {
-        value: isize,
+    Variable(Variable),
+    FunctionCall {
+        expression: Box<Expression<'a>>,
+        args: Vec<Expression<'a>>,
     },
-    Boolean {
-        value: bool,
+
+    UnaryMinus {
+        expression: Box<Expression<'a>>,
     },
+    BitwiseNot {
+        expression: Box<Expression<'a>>,
+    },
+    Length {
+        expression: Box<Expression<'a>>,
+    },
+    Not {
+        expression: Box<Expression<'a>>,
+    },
+
     Addition {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
     Subtraction {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
     Multiplication {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
     Division {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    IsGreaterThan {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+    FloorDivision {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    IsLessThan {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+    Exponentiation {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    IsGreaterOrEqual {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+    Modulo {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    IsLessOrEqual {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+
+    BitwiseAnd {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    IsEqual {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+    BitwiseOr {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    IsNotEqual {
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+    BitwiseXor {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    Negate {
-        value: Box<Expression>,
+    BitwiseLeftShift {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    ToggleSignal {
-        value: Box<Expression>,
+    BitwiseRightShift {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
-    KeepSignal {
-        value: Box<Expression>,
+
+    Concatenation {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+
+    GreaterThan {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    LessThan {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    GreaterThanOrEqual {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    LessThanOrEqual {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    Equals {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    Different {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    And {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
+    },
+    Or {
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
 }
+
+#[derive(Debug, PartialEq)]
+pub struct Number;
+
+#[derive(Debug, PartialEq)]
+pub struct Variable;
+
+#[derive(Debug, PartialEq)]
+pub struct Table;

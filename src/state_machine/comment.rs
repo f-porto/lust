c7ea77af2b22d31db346771593de3feb0163870c
f-porto <felipe.portosa@gmail.com>
2023-02-23 22:48:01 -0300

    impl state trait

--- src/state_machine/comment.rs
+++ src/state_machine/comment.rs
@@ -1,4 +1,4 @@
-use super::StateMachine;
+use super::{State, StateMachine, Symbol};
 
 #[derive(Debug)]
 pub enum CommentState {
@@ -13,6 +13,7 @@ pub enum CommentState {
     End,
 }
 
+// --[[]]
 #[derive(Debug)]
 pub struct CommentStateMachine {
     state: CommentState,
@@ -22,6 +23,29 @@ impl CommentStateMachine {
     pub fn new() -> Self {
         Self {
             state: CommentState::Initial,
+        }
+    }
+}
+
+impl State<char> for CommentState {
+    fn is_final(&self) -> bool {
+        matches!(
+            self,
+            Self::SecondDash | Self::AnythingLine | Self::SecondLeftBracket | Self::End
+        )
+    }
+
+    fn expects(&self) -> Vec<Symbol<char>> {
+        match self {
+            CommentState::Initial => vec![Symbol::One('-')],
+            CommentState::FirstDash => vec![Symbol::one('-')],
+            CommentState::SecondDash => vec![Symbol::Any],
+            CommentState::FirstLeftBracket => vec![Symbol::Any],
+            CommentState::SecondLeftBracket => vec![Symbol::Any],
+            CommentState::FistRightBracket => vec![Symbol::Any],
+            CommentState::AnythingLine => vec![Symbol::Any],
+            CommentState::AnythingBlock => vec![Symbol::Any],
+            CommentState::End => vec![],
         }
     }
 }

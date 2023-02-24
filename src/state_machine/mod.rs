c86352e6559022b11a7cf8379a9f2a2f4b162299
f-porto <felipe.portosa@gmail.com>
2023-02-23 22:47:50 -0300

    trait for states

--- src/state_machine/mod.rs
+++ src/state_machine/mod.rs
@@ -1,13 +1,23 @@
-pub mod outer;
-pub mod number;
 pub mod comment;
+pub mod number;
+pub mod outer;
 pub mod string;
 
-pub trait StateMachine<State, Symbol>: Sized {
-    fn next(&mut self, symbol: Symbol) -> bool;
+pub trait StateMachine<SMState: State<SMSymbol>, SMSymbol>: Sized {
+    fn next(&mut self, symbol: SMSymbol) -> bool;
 
-    fn state(&self) -> &'_ State;
+    fn state(&self) -> &'_ SMState;
 
     fn reset(&mut self);
 }
 
+pub enum Symbol<T> {
+    One(T),
+    Any,
+}
+
+pub trait State<T> {
+    fn is_final(&self) -> bool;
+
+    fn expects(&self) -> Vec<Symbol<T>>;
+}

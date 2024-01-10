d367e4b64238f769ed436aca6a6aa89a6a5669c2
f-porto <felipe.portosa@gmail.com>
2024-01-09 23:34:42 -0300

    removing old parser shit and add pest

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,126 @@
-mod ast;
-mod error;
-mod expression_tree;
-mod lexer;
-mod parser;
-mod state_machine;
-mod token;
-mod expression_parser;
+use pest_derive::Parser;
+
+#[derive(Debug, Parser)]
+#[grammar = "lua.pest"]
+pub struct LuaParser;
+
+#[cfg(test)]
+mod tests {
+    use std::error::Error;
+
+    use pest::Parser;
+
+    use crate::{LuaParser, Rule};
+    use pretty_assertions::assert_eq;
+
+    fn check(expected: &[&str], rule: Rule) -> Result<(), Box<dyn Error>> {
+        let text = expected.join(",");
+        let pairs = LuaParser::parse(rule, &text);
+        let Ok(mut pairs) = pairs else {
+            panic!("{}", pairs.err().unwrap());
+        };
+        let pairs: Vec<_> = pairs.next().unwrap().into_inner().collect();
+
+        println!("{:?}", pairs);
+        assert_eq!(pairs.len() - 1, expected.len());
+        for i in 0..expected.len() {
+            assert_eq!(pairs[i].as_str(), expected[i]);
+        }
+        Ok(())
+    }
+
+    #[test]
+    fn read_integers() -> Result<(), Box<dyn Error>> {
+        let integers = ["0", "12345", "-0", "-123455"];
+        check(&integers, Rule::integers)
+    }
+
+    #[test]
+    fn read_hex_integers() -> Result<(), Box<dyn Error>> {
+        let integers = ["0x0", "0x123A5", "-0xF", "-0x123CD55"];
+        check(&integers, Rule::hex_integers)
+    }
+
+    #[test]
+    fn read_floats() -> Result<(), Box<dyn Error>> {
+        let floats = [
+            "12.",
+            "-34.",
+            ".02",
+            "-.12",
+            "34.e-3",
+            "54.e45",
+            "-43.e23",
+            "-6.e-1",
+            "7e3",
+            "-7e3",
+            "324.1231",
+            "-432.423e34",
+        ];
+        check(&floats, Rule::floats)
+    }
+
+    #[test]
+    fn read_hex_floats() -> Result<(), Box<dyn Error>> {
+        let floats = [
+            "0x12.",
+            "-0xf4.",
+            "0x.a2",
+            "-0x.12",
+            "0x34.p-3",
+            "0x54.pc5",
+            "-0x43f.p23",
+            "-0x6.p-1",
+            "0x7p3",
+            "-0x7p3",
+            "0xf324.1231",
+            "-0x432.423p3a4",
+        ];
+        check(&floats, Rule::hex_floats)
+    }
+
+    #[test]
+    fn read_identifiers() -> Result<(), Box<dyn Error>> {
+        let ids = ["asads", "a", "_", "as8ads", "_232"];
+        check(&ids, Rule::identifiers)
+    }
+
+    #[test]
+    fn read_sq_strings() -> Result<(), Box<dyn Error>> {
+        let strings = [
+            r#"'s'"#,
+            r#"'Hello, World!'"#,
+            r#"'   '"#,
+            r#"'  Hello, \97 World!  '"#,
+            r#"''"#,
+            r#"'\''"#,
+            r#"'\\'"#,
+        ];
+        check(&strings, Rule::sq_strings)
+    }
+
+    #[test]
+    fn read_dq_strings() -> Result<(), Box<dyn Error>> {
+        let strings = [
+            r#""s""#,
+            r#""Hello, World!""#,
+            r#""   ""#,
+            r#""  Hello, \97 World!  ""#,
+            r#""""#,
+            r#""\"""#,
+            r#""\\""#,
+        ];
+        check(&strings, Rule::dq_strings)
+    }
+
+    #[test]
+    fn read_raw_strings() -> Result<(), Box<dyn Error>> {
+        let strings = [
+            r#"[[]]"#,
+            r#"[[Hello, World]]"#,
+            "[===[]===]",
+            "[===[ Hello [=[World]=] ]===]",
+        ];
+        check(&strings, Rule::raw_strings)
+    }
+}

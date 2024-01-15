25cd5d1529ecc9643caff6f1bef83a1ef1bbf863
f-porto <felipe.portosa@gmail.com>
2024-01-14 23:30:19 -0300

    moving print functions

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,27 @@ pub mod ast;
 mod expression;
 mod prefix_expression;
 mod statement;
+use pest::iterators::{Pair, Pairs};
 use pest_derive::Parser;
 
 #[derive(Debug, Parser)]
 #[grammar = "lua.pest"]
 pub struct LuaParser;
+
+
+fn print_pair(pair: &Pair<Rule>) {
+    println!(
+        "{:?} ({}): {:?}",
+        pair.as_rule(),
+        pair.as_node_tag().unwrap_or("x"),
+        pair.as_str()
+    );
+}
+
+fn print_pairs(pairs: Pairs<Rule>, ident: usize) {
+    for pair in pairs {
+        print!("{}", " ".repeat(ident));
+        print_pair(&pair);
+        print_pairs(pair.into_inner(), ident + 2);
+    }
+}

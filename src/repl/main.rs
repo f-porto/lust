5b3ae9f0ab0ec77124531562ee1482278a3283a6
f-porto <felipe.portosa@gmail.com>
2024-03-24 22:45:50 -0300

    Nothing really, just changed the print

--- src/repl/main.rs
+++ src/repl/main.rs
@@ -12,7 +12,7 @@ fn main() {
     stdin.read_to_string(&mut content).unwrap();
     let pairs = LuaParser::parse(Rule::Chunk, &content);
     let Ok(mut pairs) = pairs else {
-        println!("{}", pairs.err().unwrap());
+        println!("Error: {}", pairs.err().unwrap());
         return;
     };
     let program = build_ast(&mut pairs);

61c793c37a3e2ce6b6e24fc2efec048a15dd0f2d
f-porto <felipe.portosa@gmail.com>
2024-03-25 20:13:29 -0300

    Added some operators to values and some type coercions

--- src/interpreter/value.rs
+++ src/interpreter/value.rs
@@ -1,18 +1,23 @@
-use std::{collections::HashMap, hash::Hash};
+use std::{collections::HashMap, error::Error, fmt::Debug, hash::Hash};
 
-use crate::parser::{
-    expression::{Expression, Field},
+use pest::Parser;
+
+use crate::{parser::{
+    expression::{self, Expression, Field},
     statement::{Block, Parameters},
-};
+    LuaParser, Rule,
+}, std::Builtin};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Table {
     counter: usize,
     table: HashMap<Value, Value>,
 }
 
 impl Table {
-    pub fn insert(&mut self, key: &Value, value: &Value) {}
+    pub fn insert(&mut self, key: &Value, value: &Value) {
+        self.table.insert(key.clone(), value.clone());
+    }
 
     pub fn get(&self, key: &Value) -> Option<&Value> {
         self.table.get(key)
@@ -57,7 +62,7 @@ impl Table {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Value {
     Nil,
     False,
@@ -67,11 +72,107 @@ pub enum Value {
     String(String),
     Table(Table),
     Lambda { parameters: Parameters, body: Block },
+    Builtin(Builtin),
 }
 
 impl Value {
     pub fn is_truthy(self) -> bool {
         !matches!(self, Value::False | Value::Nil)
+    }
+
+    pub fn to_number(&self) -> Result<Self, Box<dyn Error>> {
+        match self {
+            Value::Float(_) | Value::Integer(_) => Ok(self.clone()),
+            Value::String(s) => {
+                let pairs = LuaParser::parse(Rule::Number, &s)?;
+                let pair = pairs.into_iter().next().unwrap();
+                match pair.as_rule() {
+                    Rule::Integer => Ok(Value::Integer(expression::parse_integer(&s))),
+                    Rule::HexInteger => Ok(Value::Integer(expression::parse_hex_integer(&s))),
+                    Rule::Float => Ok(Value::Float(expression::parse_float(&s))),
+                    Rule::HexFloat => Ok(Value::Float(expression::parse_hex_float(&s))),
+                    p => unreachable!("Expected number, found {:?}", p),
+                }
+            }
+            _ => Err(String::from(""))?,
+        }
+    }
+
+    pub fn to_float(&self) -> Result<Self, Box<dyn Error>> {
+        match self.to_number()? {
+            Value::Float(f) => Ok(Value::Float(f)),
+            Value::Integer(n) => Ok(Value::Float(n as f64)),
+            v => unreachable!("Expected number, found {:?}", v),
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        match self {
+            Value::Nil => "nil".to_string(),
+            Value::False => "false".to_string(),
+            Value::True => "true".to_string(),
+            Value::Integer(n) => n.to_string(),
+            Value::Float(f) => f.to_string(),
+            Value::String(s) => s.clone(),
+            Value::Table(_) => todo!(),
+            Value::Lambda { parameters, body } => todo!(),
+            Value::Builtin(_) => todo!(),
+        }
+    }
+}
+
+impl Value {
+    pub fn add(&self, rhs: &Self) -> Result<Value, Box<dyn Error>> {
+        let lhs = self.to_number()?;
+        let rhs = rhs.to_number()?;
+        let v = match (lhs, rhs) {
+            (Value::Integer(a), Value::Integer(b)) => Value::Integer(a + b),
+            (Value::Integer(a), Value::Float(b)) => Value::Float(a as f64 + b),
+            (Value::Float(a), Value::Integer(b)) => Value::Float(a + b as f64),
+            (Value::Float(a), Value::Float(b)) => Value::Float(a + b),
+            (lhs, rhs) => unreachable!("Expected numbers, found {:?} and {:?}", lhs, rhs),
+        };
+        Ok(v)
+    }
+
+    pub fn is_equal(&self, rhs: &Self) -> bool {
+        match (self, rhs) {
+            (Value::Nil, Value::Nil) => true,
+            (Value::False, Value::False) => true,
+            (Value::True, Value::True) => true,
+            (Value::Integer(a), Value::Integer(b)) => a == b,
+            (Value::Integer(a), Value::Float(b)) => &(*a as f64) == b,
+            (Value::Float(a), Value::Integer(b)) => a == &(*b as f64),
+            (Value::Float(a), Value::Float(b)) => a == b,
+            (Value::String(a), Value::String(b)) => a == b,
+            (Value::Table(_), Value::Table(_)) => todo!(),
+            (Value::Lambda { .. }, Value::Lambda { .. }) => todo!(),
+            (_, _) => false,
+        }
+    }
+
+    pub fn is_less_than(&self, rhs: &Self) -> Result<bool, Box<dyn Error>> {
+        let r = match (self, rhs) {
+            (Value::Integer(a), Value::Integer(b)) => a < b,
+            (Value::Integer(a), Value::Float(b)) => &(*a as f64) < b,
+            (Value::Float(a), Value::Integer(b)) => a < &(*b as f64),
+            (Value::Float(a), Value::Float(b)) => a < b,
+            (Value::String(a), Value::String(b)) => a < b,
+            (_, _) => return Err("")?,
+        };
+        Ok(r)
+    }
+
+    pub fn is_less_or_equal(&self, rhs: &Self) -> Result<bool, Box<dyn Error>> {
+        let r = match (self, rhs) {
+            (Value::Integer(a), Value::Integer(b)) => a <= b,
+            (Value::Integer(a), Value::Float(b)) => &(*a as f64) <= b,
+            (Value::Float(a), Value::Integer(b)) => a <= &(*b as f64),
+            (Value::Float(a), Value::Float(b)) => a <= b,
+            (Value::String(a), Value::String(b)) => a <= b,
+            (_, _) => return Err("")?,
+        };
+        Ok(r)
     }
 }
 
